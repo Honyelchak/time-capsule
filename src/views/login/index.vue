421803a1d0c39f4ef31754126d49089ede7f989b
honyelchak <554417388@qq.com>
2024-07-23 07:54:17 +0800

    init project

---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+<template>
+    <el-row>
+        <el-col :span="6">
+            <img src="" />
+        </el-col>
+        <el-col :span="6">asdasd</el-col>
+    </el-row>
+</template>
+
+<style></style>
